3ffea6f7a25797874108d7e3ee5d01d2ebdd53b2
Mayank Sharma <129668160+MayankOg@users.noreply.github.com>
2026-04-29 16:57:46 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   vite: {
-    base: "/your-repo-name/", // 🔥 REQUIRED for GitHub Pages
+    base: "/shiva-core-systems/", // 🔥 REQUIRED for GitHub Pages
   },
 });
